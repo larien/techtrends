6d73c94de885898f9a4743a5d6d6393ca22b67a5
Lauren Ferreira <lauren.ferreira@fox.com>
2022-02-03 16:23:14 -0300

    feat: include healthcheck

--- techtrends/app.py
+++ techtrends/app.py
@@ -65,6 +65,11 @@ def create():
 
     return render_template('create.html')
 
+@app.route('/healthz', methods='GET')
+def healthz():
+    result = { 'result': 'OK - healthy' }
+    return jsonify(result)
+
 # start the application on port 3111
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port='3111')
